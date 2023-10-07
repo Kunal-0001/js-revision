52ded525f878eeef5935ba9100d5584fd8ed0648
Kunal Singh Rana <kunalsinghrana4@gmail.com>
2023-10-07 16:30:30 +0000

    variables done

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,7 @@
+const accountId=2110990811;
+let accountName="Kunal Singh";
+var accountPassword="12345";
+accountCity="Chandigarh";
+let accountState;
+
+console.table([accountId,accountName,accountPassword,accountCity,accountState]);
