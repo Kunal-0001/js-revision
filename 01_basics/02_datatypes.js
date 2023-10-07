4c6c4f51f95cde5a4139408d537f0692ccd25537
Kunal Singh Rana <kunalsinghrana4@gmail.com>
2023-10-07 16:44:37 +0000

    datatypes.done

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,11 @@
+let name="kunal"
+let age=20
+let state=true
+let abc;
+let def=null;
+
+console.log(typeof name);
+console.log(typeof age);
+console.log(typeof state);
+console.log(typeof abc);
+console.log(typeof def);
